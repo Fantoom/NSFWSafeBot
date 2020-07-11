a949c0bff20e34f49f35632441beb450395cb86b
GitLab <23425714+Jeidoz@users.noreply.github.com>
2020-07-11 23:49:35 +0300

    Refactored DB access and async methods

--- NSFWSafeBot/DBController.cs
+++ NSFWSafeBot/DBController.cs
@@ -1,53 +1,54 @@
 ﻿using LiteDB;
-using System;
-using System.Linq;
+using NSFWSafeBot.Models;
 using System.Collections.Generic;
-using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace NSFWSafeBot
 {
-	static class DBController
-	{
-		public static LiteDatabase db { get; private set; }
-		public static ILiteCollection<Message> Messages { get; private set; }
+    internal static class DBController
+    {
+        private const string DatabaseFileName = "messages.db";
 
-		public static void Init()
-		{
-			db = new LiteDatabase(Path.Combine(Directory.GetCurrentDirectory(), "messages.db"));
-			Messages = db.GetCollection<Message>("messages");
-		}
+        public static LiteDatabase Database { get; private set; }
+        public static ILiteCollection<ChatMessage> Messages { get; private set; }
 
-		public static BsonValue AddMessage(Message message)
-		{
-			return Messages.Insert(message);
-		}
-		public static bool DeleteMessageById(long chatId, int messageId)
-		{
-			var message = Messages.Query().ToList().Where(x => x.ChatId == chatId && x.MessageId == messageId).FirstOrDefault();
-			return Messages.Delete(message.Id);
-		}
+        static DBController()
+        {
+            string databasePath = Path.Combine(Directory.GetCurrentDirectory(), DatabaseFileName);
+            Database = new LiteDatabase(databasePath);
+            Messages = Database.GetCollection<ChatMessage>("messages");
+        }
 
-		public static List<Message> GetMessagesList(int userId)
-		{
-			return Messages.Query().ToList().Where(x => x.UserId == userId).ToList();
-		}
+        public static BsonValue AddMessage(ChatMessage message)
+        {
+            return Messages.Insert(message);
+        }
 
-		public static async Task<List<Message>> GetMessagesListAsync(int userId)
-		{
-			return await Task.Run(() => GetMessagesList(userId));
-		}
+        public static bool DeleteMessageById(long chatId, int messageId)
+        {
+            var message = Messages.FindOne(msg => msg.ChatId == chatId && msg.MessageId == messageId);
+            return Messages.Delete(message.Id);
+        }
 
-		public static async Task<BsonValue> AddMessageAsync(Message message)
-		{
-			return await Task.Run(() => AddMessage(message));
-		}
-		public static async Task<bool> DeleteMessageByIdAsync(long chatId, int messageId)
-		{
-			return await Task.Run(() => DeleteMessageById(chatId, messageId));
-		}
+        public static IEnumerable<ChatMessage> GetUserMessages(int userId)
+        {
+            return Messages.Find(msg => msg.UserId == userId);
+        }
 
+        public static Task<IEnumerable<ChatMessage>> GetUserMessagesAsync(int userId)
+        {
+            return Task.Run(() => GetUserMessages(userId));
+        }
 
-	}
+        public static Task<BsonValue> AddMessageAsync(ChatMessage message)
+        {
+            return Task.Run(() => AddMessage(message));
+        }
+
+        public static Task<bool> DeleteMessageByIdAsync(long chatId, int messageId)
+        {
+            return Task.Run(() => DeleteMessageById(chatId, messageId));
+        }
+    }
 }
