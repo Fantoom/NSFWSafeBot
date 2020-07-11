9670c7a46ef67a794bf4ea9eb30460a0a190d54e
GitLab <23425714+Jeidoz@users.noreply.github.com>
2020-07-11 23:50:01 +0300

    Added new help abstractions

---
+++ NSFWSafeBot/Commands/Data/CallbackQueryContent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Telegram.Bot.Types;
+
+namespace NSFWSafeBot.Commands.Data
+{
+    public sealed class CallbackQueryContent
+    {
+        private const int ChatIdPartOrderNumber = 0;
+        private const int MessageIdPartOrderNumber = 1;
+
+        public string CallbackId;
+        public long ForwardToChatId;
+        public int MessageId;
+        public int ChatId;
+
+        public CallbackQueryContent(CallbackQuery callbackQuery)
+        {
+            CallbackId = callbackQuery.Id;
+            ChatId = callbackQuery.From.Id;
+            ParseCallbackDataContent(callbackQuery.Data);
+        }
+
+        private void ParseCallbackDataContent(string callbackData)
+        {
+            string[] callbackDataParts = callbackData.Split('|', StringSplitOptions.RemoveEmptyEntries);
+            ForwardToChatId = long.Parse(callbackDataParts[ChatIdPartOrderNumber]);
+            MessageId = int.Parse(callbackDataParts[MessageIdPartOrderNumber]);
+        }
+    }
+}
---
+++ NSFWSafeBot/Models/ChatMessage.cs
@@ -0,0 +1,13 @@
+﻿using Telegram.Bot.Types;
+
+namespace NSFWSafeBot.Models
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public long ChatId { get; set; }
+        public int MessageId { get; set; }
+        public Message Message { get; set; }
+    }
+}
