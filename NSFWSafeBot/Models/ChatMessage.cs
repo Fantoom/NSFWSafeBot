6931a7df26a7fdd6f829b26e244fe07cf38d60eb
Stanislav Khavruk <stas_khavruk@outlook.com>
2020-07-13 14:03:38 +0300

    Refactored bot (#1)
    * Added config example
    
    * Refactored DB access and async methods
    
    * Added new help abstractions
    
    * Code style fix
    
    * Code style and async refactoring
    
    * Refactored bot client
    
    * Added polling and refactored config init
    
    * Fixed not showed images and implemented search by caption

---
+++ NSFWSafeBot/Models/ChatMessage.cs
@@ -0,0 +1,29 @@
+﻿using Telegram.Bot.Types;
+
+namespace NSFWSafeBot.Models
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public long ChatId { get; set; }
+        public int MessageId { get; set; }
+        public string Caption { get; set; }
+        public PhotoSize Photo { get; set; }
+
+        // ReSharper disable once UnusedMember.Global
+        // Uses by LiteDbCollection
+        public ChatMessage()
+        {
+        }
+
+        public ChatMessage(Message message)
+        {
+            ChatId = message.Chat.Id;
+            UserId = message.From.Id;
+            MessageId = message.MessageId;
+            Caption = message.Caption?.ToLower() ?? string.Empty;
+            Photo = message.Photo?[0];
+        }
+    }
+}
---
+++ NSFWSafeBot/NSFMBot.cs
@@ -0,0 +1,173 @@
+﻿using NSFWSafeBot.Commands;
+using NSFWSafeBot.Commands.Data;
+using NSFWSafeBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.InlineQueryResults.Abstractions;
+
+namespace NSFWSafeBot
+{
+    public sealed class NsfmBot
+    {
+        private readonly string _token;
+        private readonly List<ICommand> _commands;
+
+        public static TelegramBotClient BotClient { get; private set; }
+
+        public NsfmBot(string token)
+        {
+            _token = token;
+            InitializeBotCommands(out _commands);
+            InitializeBotClient();
+        }
+
+        private void InitializeBotCommands(out List<ICommand> commands)
+        {
+            commands = new List<ICommand>
+            {
+                new DeleteMessage(),
+                new Command
+                {
+                    CommandName = "start",
+                    Action = (msg) =>
+                    {
+                        BotClient.SendTextMessageAsync(msg.Chat.Id, "Write NSFW message to show in inline mode ");
+                    }
+                }
+            };
+        }
+
+        private void InitializeBotClient()
+        {
+            BotClient = new TelegramBotClient(_token);
+            BotClient.OnMessage += BotClient_OnMessage;
+            BotClient.OnInlineQuery += BotClient_OnInlineQuery;
+            BotClient.OnCallbackQuery += BotClient_OnCallbackQuery;
+        }
+
+        private async void BotClient_OnCallbackQuery(object sender, CallbackQueryEventArgs e)
+        {
+            var callBackContent = new CallbackQueryContent(e.CallbackQuery);
+            await TryToForwardMessageAsync(callBackContent);
+        }
+
+        private Task TryToForwardMessageAsync(CallbackQueryContent callBackContent)
+        {
+            try
+            {
+                return BotClient.ForwardMessageAsync(
+                    callBackContent.ChatId,
+                    callBackContent.ForwardToChatId,
+                    callBackContent.MessageId);
+            }
+            catch (ChatNotInitiatedException)
+            {
+                return BotClient.AnswerCallbackQueryAsync(
+                    callBackContent.CallbackId,
+                    "First write to the bot.",
+                    true);
+            }
+            catch (Exception)
+            {
+                return BotClient.AnswerCallbackQueryAsync(
+                    callBackContent.CallbackId,
+                    "Unpredicted error happened.",
+                    true);
+            }
+        }
+
+        private async void BotClient_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.Message.Text) && e.Message.Photo is null)
+            {
+                return;
+            }
+
+            if (!IsCommand(e.Message) && e.Message.Photo != null)
+            {
+                await DBController.AddMessageAsync(new ChatMessage(e.Message));
+            }
+            else if (IsCommand(e.Message))
+            {
+                var command = _commands.FirstOrDefault(x => x.CommandName == GetCommandName(e.Message));
+                if (command is null)
+                {
+                    await BotClient.SendTextMessageAsync(e.Message.Chat.Id, "Unknown command");
+                    return;
+                }
+
+                await command.ExecuteAsync(e.Message);
+            }
+            else
+            {
+                await BotClient.SendTextMessageAsync(e.Message.Chat.Id, "Send command or image, please");
+            }
+        }
+
+        private bool IsCommand(Message message)
+        {
+            return !string.IsNullOrWhiteSpace(message.Text)
+                   && message.Text.StartsWith("/");
+        }
+
+        private string GetCommandName(Message message)
+        {
+            // Skip slash character that comes first
+            return message.Text.Substring(1);
+        }
+
+        private async void BotClient_OnInlineQuery(object sender, InlineQueryEventArgs e)
+        {
+            IEnumerable<ChatMessage> userMessages =
+                await DBController.GetUserMessagesAsync(e.InlineQuery.From.Id, e.InlineQuery.Query);
+
+            var results = new List<InlineQueryResultBase>();
+            uint resultId = 0;
+            foreach (var userChatMessage in userMessages)
+            {
+                var photo = await BotClient.GetFileAsync(userChatMessage.Photo.FileId);
+                var newArticle = new InlineQueryResultCachedPhoto(resultId.ToString(), photo.FileId);
+                results.Add(newArticle);
+                resultId++;
+            }
+
+            try
+            {
+                await BotClient.AnswerInlineQueryAsync(
+                    inlineQueryId: e.InlineQuery.Id,
+                    results: results,
+                    isPersonal: true,
+                    cacheTime: 0);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private async Task AddArticleThumb(IThumbnailInlineQueryResult article, PhotoSize messagePhoto)
+        {
+            var photo = await BotClient.GetFileAsync(messagePhoto.FileId);
+            article.ThumbUrl = $"https://api.telegram.org/file/bot{_token}/{photo.FilePath}";
+            article.ThumbHeight = 100;
+            article.ThumbWidth = 100;
+        }
+
+        public void StartPolling()
+        {
+            BotClient.StartReceiving();
+        }
+
+        public void StopPolling()
+        {
+            BotClient.StopReceiving();
+        }
+    }
+}
